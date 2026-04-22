0c2dcaac3c60f16d1bfaf9721c83bbc5a977239b
Vishal Kumar Jain <vishal_kumarjain@epam.com>
2026-04-22 20:10:02 +0530

    wip:current progress

---
+++ src/main-spa.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { SlideShow } from "./components/SlideShow";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <SlideShow />
+  </React.StrictMode>
+);
